16ccb3b01c7f9b2c31ab38c7b746a926e1dc04d5
harshkeshwala <keshwalaharsh@gmail.com>
2018-12-06 14:57:43 -0500

    Tasks adding and deleting added

--- ToDoListApp/RootViewController.swift
+++ ToDoListApp/RootViewController.swift
@@ -11,115 +11,179 @@ import CoreData
 
 //var list = ["A", "B", "C", "D"]
 
-class RootViewController: UITableViewController {
-
-    var tasks = [ToDoEntity]()
+class RootViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource
+{
+    var tasks: [NSManagedObject] = []
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     @IBOutlet weak var tasksList: UITableView!
     
-    @IBAction func addNewTask(_ sender: UIBarButtonItem)
+    override func viewDidLoad()
     {
-        showInputDialog()
-    }
-    override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchData = NSFetchRequest<NSManagedObject>(entityName: "Entity")
+        
+        do
+        {
+            tasks = try context.fetch(fetchData)
+        }
+        catch
+        {
+            print("Error loading the data")
+        }
+    }
+    
+ 
+    
+    var titleTextField: UITextField!
+    var descriptionTextField: UITextField!
+    
+    func titleTextField(textfield: UITextField!)
+    {
+        titleTextField = textfield
+        titleTextField.placeholder = "Add Title"
+    }
+    
+    func descriptionTextField(textfield: UITextField!)
+    {
+        descriptionTextField = textfield
+        descriptionTextField.placeholder = "Add Description"
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    
+
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//
+//        var t = self.tasks[indexPath.row]
+//        t.completed = !t.completed
+//        saveTasks
+//    }
+
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt: IndexPath) -> [UITableViewRowAction]? {
+
+//        let finished = UITableViewRowAction(style: .normal, title: "Mark Finished") { action, indexPath in
+//            //print("more button tapped")
+//            self.tasksList.deselectRow(at: indexPath, animated: true)
+//
+//            var t = self.tasks[indexPath.row]
+//            t.completed = !t.completed
+//            self.saveTasks(alert: UIAlertAction)
+//        }
+//        finished.backgroundColor = .lightGray
+
+        let delete = UITableViewRowAction(style: .normal, title: "Delete") { action, indexPath in
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+
+            context.delete(self.tasks[indexPath.row])
+            self.tasks.remove(at: indexPath.row)
+
+            do
+            {
+                try context.save()
+            }
+            catch
+            {
+                print("Error occured while deleting!")
+            }
+            self.tasksList.reloadData()
+        }
+        delete.backgroundColor = .red
+
+        return [delete]
+    }
+
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+   
+    
+    @IBAction func addNewTask(_ sender: UIBarButtonItem)
+    {
+        let alert = UIAlertController(title: "Add Task", message: "Add New Task Below", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Save", style: .default, handler: self.saveTasks)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        alert.addAction(action)
+        alert.addAction(cancelAction)
+        
+        alert.addTextField(configurationHandler: titleTextField)
+        alert.addTextField(configurationHandler: descriptionTextField)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveTasks(alert: UIAlertAction)
+    {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let entity = NSEntityDescription.entity(forEntityName: "Entity", in: context)!
+        
+            let theTitle = NSManagedObject(entity: entity, insertInto: context)
+            theTitle.setValue(titleTextField.text, forKey: "name")
+        
+            let theDesc = NSManagedObject(entity: entity, insertInto: context)
+            theDesc.setValue(descriptionTextField.text, forKey: "notes")
+        
+            do
+            {
+                try context.save()
+                tasks.append(theTitle)
+                tasks.append(theDesc)
+            }
+            catch
+            {
+                print("Error occured while saving")
+            }
+            self.tasksList.reloadData()
+    }
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+//    {
+//        if editingStyle == UITableViewCellEditingStyle.delete
+//        {
+//            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//            let context = appDelegate.persistentContainer.viewContext
+//
+//            context.delete(tasks[indexPath.row])
+//            tasks.remove(at: indexPath.row)
+//
+//            do
+//            {
+//                try context.save()
+//            }
+//            catch
+//            {
+//                print("Error occured while deleting!")
+//            }
+//            self.tasksList.reloadData()
+//        }
+//    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         //return (list.count)
         return (tasks.count)
     }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let task = tasks[indexPath.row]
-        cell.textLabel?.text = task.name
-        
-        cell.accessoryType = task.completed ? .checkmark : .none
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        tasks[indexPath.row].completed = !tasks[indexPath.row].completed
-        saveTasks()
-    }
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let task = tasks[indexPath.row]
-            tasks.remove(at: indexPath.row)
-            context.delete(task)
-            
-            do{
-                try context.save()
-            }catch{
-                print("Error Deleting Task \(error)")
-            }
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-            
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        tasksList.reloadData()
-    }
-  
-    func showInputDialog() {
-        var textField = UITextField()
-        let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Save", style: .default) { (action) in
-            
-            let newTask = ToDoEntity(context: self.context)
-            newTask.name = textField.text!
-            self.tasks.append(newTask)
-            self.saveTasks()
-        }
-        
-        alert.addAction(action)
-        alert.addTextField { (field) in
-            textField = field
-            textField.placeholder = "Add a New Todo Task"
-        }
-        
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func saveTasks(){
-        do{
-            try context.save()
-        }catch{
-            print("Error Saving Task")
-        }
-        tableView.reloadData()
-    }
-    
-    func loadTasks(){
-        
-        let request: NSFetchRequest<ToDoEntity> = ToDoEntity.fetchRequest()
-        
-        do{
-            tasks = try context.fetch(request)
-        }catch{
-            print("Error fetching tasks \(error)")
-        }
-        
-        tableView.reloadData()
+        let title = tasks[indexPath.row]
+        let cell = tasksList.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = title.value(forKey: "name") as? String
+        cell.detailTextLabel?.text = title.value(forKey: "notes") as? String
+        return cell
     }
 }
